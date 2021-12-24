d6a4be4a455c73d0c2d9e40cc51c9d344e4e69c7
Вадим <nerzhularthas@gmail.com>
2021-12-25 00:09:53 +0600

    Created simple build script

---
+++ src/build.ps1
@@ -0,0 +1,42 @@
+$output = $args[0]
+
+if ($null -eq $output -or "" -eq $output) {
+    Write-Output "output path is empty"
+    exit
+}
+Write-Output "output path: " $output 
+
+$of = New-Item -Path . -Name $output -ItemType "file" -Force
+
+if (-not $of.Exists) {
+    Write-Output "output file not created"
+    exit
+}
+
+foreach($line in Get-Content .\index.html) {
+    $st = -1
+    do {
+        $st = $line.IndexOf("{", $st + 1)
+        if (-1 -eq $st) {
+            Add-Content -Path $output -Value $line
+            break        
+        }
+        $en = $line.IndexOf("}", $st + 1)
+        if (-1 -eq $st) {
+            Add-Content -Path $output -Value $line
+            break        
+        }
+        Add-Content -Path $output -Value $line.SubString(0, $st)
+        
+        $subFile = $line.SubString($st + 1, $en - $st - 1)
+
+        foreach($subLine in Get-Content $subFile) {
+            Add-Content -Path $output -Value $subLine
+        }
+
+        $line = $line.Substring($en + 1)
+        $st = -1
+    } while (1)
+}
+
+Write-Output "done"
